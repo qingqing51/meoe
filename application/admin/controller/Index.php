d121bf539f9e5236d7fd3089c2ea462c5def962b
wei1co <wei1co@192.168.0.108>
2018-10-09 15:55:21 +0800

    重新提交

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return view();
+    }
+    
+    
+    public function db(){
+        
+        
+        
+        
+    }
+    
+}
